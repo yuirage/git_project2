91c225e2e00c8e1bc300d9e7140c6c5986e22418
Болдова Полина <pol1na.boldova@yandex.ru>
2023-11-22 15:28:37 +0300

    3 commit

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
+def my_function():
+    print("Нужно решать задачи!")
+
+
 def main():
     print('My first git program')
     print('And I change it every day')
+    my_function()()
 
 
 if __name__ == '__main__':
